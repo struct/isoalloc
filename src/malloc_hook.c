dbfb690fc5dabf22f06bdd8aced93eb9bea35fc4
struct <chris.rohlf@gmail.com>
2020-03-21 12:08:44 -0400

    better malloc hook, still unsupported

--- src/malloc_hook.c
+++ src/malloc_hook.c
@@ -10,38 +10,80 @@
  * iso_alloc interfaces directly. These hook points are
  * useful because they allow us to use iso_alloc even in
  * existing and closed source programs that call malloc/free.
- * Using this requires LD_PRELOAD so it is not recommended. */
+ * Using this requires LD_PRELOAD so it is not recommended! */
 #if MALLOC_HOOK
-EXTERNAL_API void *malloc(size_t size) {
-    return iso_alloc(size);
+
+EXTERNAL_API void *__libc_malloc(size_t s) {
+    return iso_alloc(s);
+}
+
+EXTERNAL_API void *malloc(size_t s) {
+    return iso_alloc(s);
+}
+
+EXTERNAL_API void __libc_free(void *p) {
+    iso_free(p);
 }
 
 EXTERNAL_API void free(void *p) {
     iso_free(p);
-    return;
 }
 
-EXTERNAL_API void *calloc(size_t nmemb, size_t size) {
-    return iso_calloc(nmemb, size);
+EXTERNAL_API void *__libc_calloc(size_t n, size_t s) {
+    return iso_calloc(n, s);
 }
 
-EXTERNAL_API void *realloc(void *p, size_t size) {
-    return iso_realloc(p, size);
+EXTERNAL_API void *calloc(size_t n, size_t s) {
+    return iso_calloc(n, s);
 }
 
-EXTERNAL_API int posix_memalign(void **memptr, size_t alignment, size_t size) {
+EXTERNAL_API void *__libc_realloc(void *p, size_t s) {
+    return iso_realloc(p, s);
+}
+
+EXTERNAL_API void *realloc(void *p, size_t s) {
+    return iso_realloc(p, s);
+}
+
+EXTERNAL_API int __posix_memalign(void **r, size_t a, size_t s) {
     /* All iso_alloc allocations are 8 byte aligned */
-    *memptr = iso_alloc(size);
+    *r = iso_alloc(s);
 
-    if(*memptr != NULL) {
+    if(*r != NULL) {
         return 0;
     } else {
         return ENOMEM;
     }
 }
-
-EXTERNAL_API void *memalign(size_t alignment, size_t size) {
-    /* All iso_alloc allocations are 8 byte aligned */
-    return iso_alloc(size);
+EXTERNAL_API int posix_memalign(void **r, size_t alignment, size_t s) {
+    return __posix_memalign(r, alignment, s);
 }
+
+EXTERNAL_API void *__libc_memalign(size_t align, size_t s) {
+    /* All iso_alloc allocations are 8 byte aligned */
+    return iso_alloc(s);
+}
+
+EXTERNAL_API void *memalign(size_t alignment, size_t s) {
+    /* All iso_alloc allocations are 8 byte aligned */
+    return iso_alloc(s);
+}
+
+static void *libc_malloc(size_t s, const void *caller) {
+    return iso_alloc(s);
+}
+static void *libc_realloc(void *ptr, size_t s, const void *caller) {
+    return iso_realloc(ptr, s);
+}
+static void libc_free(void *ptr, const void *caller) {
+    iso_free(ptr);
+}
+static void *libc_memalign(size_t align, size_t s, const void *caller) {
+    return iso_alloc(s);
+}
+
+void *(*__malloc_hook)(size_t, const void *) = &libc_malloc;
+void *(*__realloc_hook)(void *, size_t, const void *) = &libc_realloc;
+void (*__free_hook)(void *, const void *) = &libc_free;
+void *(*__memalign_hook)(size_t, size_t, const void *) = &libc_memalign;
 #endif
