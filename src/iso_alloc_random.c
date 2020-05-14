1c0480b0cbf66d546b5965f7d78289baa396586b
struct <chris.rohlf@gmail.com>
2020-05-14 11:33:17 -0400

    ran make format

--- src/iso_alloc_random.c
+++ src/iso_alloc_random.c
@@ -8,21 +8,18 @@
 #endif
 #include "iso_alloc_internal.h"
 
+INTERNAL_HIDDEN uint64_t rand_uint64(void) {
+    uint64_t val;
 
-INTERNAL_HIDDEN uint64_t rand_uint64(void)
-{
-   uint64_t val;
-
-   // In modern versions of glibc (>=2.25) we can call getrandom(),
-   // but older versions of glibc are still in use as of writing this.
-   // Use the raw system call as a lower common denominator.
-
-   // We give up on checking the return value. The alternative would be
-   // to crash. We prefer here to keep going with degraded randomness.
-   #if __linux__
-   (void)syscall(SYS_getrandom, &val, sizeof(val), GRND_NONBLOCK);
-   #elif __APPLE__
-   (void)SecRandomCopyBytes(kSecRandomDefault, sizeof(val), &val);
-   #endif
-   return val;
+/* In modern versions of glibc (>=2.25) we can call getrandom(),
+   but older versions of glibc are still in use as of writing this.
+   Use the raw system call as a lower common denominator.
+   We give up on checking the return value. The alternative would be
+   to crash. We prefer here to keep going with degraded randomness. */
+#if __linux__
+    (void) syscall(SYS_getrandom, &val, sizeof(val), GRND_NONBLOCK);
+#elif __APPLE__
+    (void) SecRandomCopyBytes(kSecRandomDefault, sizeof(val), &val);
+#endif
+    return val;
 }
