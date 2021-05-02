c502cb5a9413abf77f07d14431e3bff6b95c2268
struct <chris.rohlf@gmail.com>
2021-05-02 19:21:12 -0400

    add use after free test

---
+++ tests/uaf.c
@@ -0,0 +1,26 @@
+/* iso_alloc uaf.c
+ * Copyright 2021 - chris.rohlf@gmail.com */
+
+#include "iso_alloc.h"
+#include "iso_alloc_internal.h"
+
+/* This test should be run manually. You need to enable UAF_PTR_PAGE
+ * and then disable the sampling logic in iso_alloc. */
+typedef struct test {
+    char *str;
+} test_t;
+
+int main(int argc, char *argv[]) {
+    void *str = iso_alloc(32);
+    test_t *test = (test_t *) iso_alloc(1024);
+    test->str = str;
+    memcpy(str, "a string!", 9);
+    iso_free(str);
+
+    /* Dereference a pointer that should have been
+     * detected and overwritten with UAF_PTR_PAGE */
+    LOG("%s", test->str);
+    iso_free(test);
+
+    return OK;
+}
