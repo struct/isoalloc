0fc75350ffce80204055667376c471f1588d4ea0
struct <chris.rohlf@gmail.com>
2021-09-05 10:43:25 -0400

    add zero allocs test

---
+++ tests/zero_alloc.c
@@ -0,0 +1,12 @@
+/* iso_alloc zero_alloc.c
+ * Copyright 2021 - chris.rohlf@gmail.com */
+
+#include "iso_alloc.h"
+#include "iso_alloc_internal.h"
+
+int main(int argc, char *argv[]) {
+    void *p = iso_alloc(0);
+    memcpy(p, "0x41", 1);
+    iso_free(p);
+    return 0;
+}
