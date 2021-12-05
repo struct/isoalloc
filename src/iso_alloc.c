2a62f9c9718435acab76f1bd06ec851d41eaed6a
struct <chris.rohlf@gmail.com>
2021-12-05 13:40:49 -0500

    remove comments from perf critical code introduced in last PR. big oops!

--- src/iso_alloc.c
+++ src/iso_alloc.c
@@ -808,9 +808,9 @@ INTERNAL_HIDDEN iso_alloc_zone *is_zone_usable(iso_alloc_zone *zone, size_t size
     /* If the cache for this zone is empty we should
      * refill it to make future allocations faster 
      * for all threads */
-    //if(zone->free_bit_slot_cache_usable >= zone->free_bit_slot_cache_index) {
-    //    fill_free_bit_slot_cache(zone);
-    //}
+    if(zone->free_bit_slot_cache_usable >= zone->free_bit_slot_cache_index) {
+        fill_free_bit_slot_cache(zone);
+    }
 
     bit_slot_t bit_slot = get_next_free_bit_slot(zone);
 
