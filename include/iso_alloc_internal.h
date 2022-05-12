15483160c2e71123f32adf8e998a6b444c0390c5
David CARLIER <devnexen@gmail.com>
2022-05-12 19:51:08 +0100

    HUGE_PAGES flags usage for macOs.
    here we use the special file descriptor to put the zone
    into a 2MB superpage.

--- include/iso_alloc_internal.h
+++ include/iso_alloc_internal.h
@@ -35,6 +35,7 @@
 #define ENVIRON environ
 #elif __APPLE__
 #include <libkern/OSByteOrder.h>
+#include <mach/vm_statistics.h>
 #define bswap_32(x) OSSwapInt32(x)
 #define bswap_64(x) OSSwapInt64(x)
 #define ENVIRON NULL
